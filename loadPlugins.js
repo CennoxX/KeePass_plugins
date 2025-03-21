a31b8b2f9e61552ef73eaedd21fbb196012c651b
CennoxX <cesar.bernard@gmx.de>
2025-03-21 23:25:01 +0100

    Update loadPlugins.js

--- loadPlugins.js
+++ loadPlugins.js
@@ -55,4 +55,4 @@ var plgs = [...doc.querySelectorAll(".tablebox")].filter(i => i.querySelector('i
 	download: pluginDownload,
 	website: website
 	}}).filter(i => i.authors && !i.id.startsWith("convertto") && i.id != "testplugin")
-await Deno.writeFile("plugins.json", JSON.stringify(plgs));
+await Deno.writeTextFile("plugins.json", JSON.stringify(plgs));
