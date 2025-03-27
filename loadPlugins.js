a3d10f5e19a5a01d17b8d4782a9e72d450ec8f10
CennoxX <cesar.bernard@gmx.de>
2025-03-27 10:15:17 +0100

    Update loadPlugins.js

--- loadPlugins.js
+++ loadPlugins.js
@@ -34,7 +34,7 @@ var plgs = [...doc.querySelectorAll(".tablebox")].filter(i => i.querySelector('i
   var title = i.querySelector("th").innerText.trim();
   var shortdescNode = doc.querySelector("[href='#"+i.getAttribute("id")+"']~br");
   var shortdesc = "";
-  while((shortdescNode = shortdescNode?.nextSibling) && shortdescNode.nodeName != "BR") {shortdesc += shortdescNode.textContent.trim() + (shortdescNode.nodeType == Node.TEXT_NODE ?  " " : "")};
+  while((shortdescNode = shortdescNode?.nextSibling) && shortdescNode.nodeName != "BR") {shortdesc += shortdescNode.textContent.trim() + (shortdescNode.nodeType == 3 ?  " " : "")};
   shortdesc = shortdesc.trim();
   var authors = extMeta?.innerText.match(/Authors?:(.*?)\. Language/s)?.[1]?.replace("\n"," ").trim();
   authors = (authors?.match(/\([^,]*\)/) ? authors.match(/\(and /) ? authors.split(/ \(and |\)/) : authors.split(/, /) : authors?.split(/ \(|\)|, /))?.filter(i => i);
