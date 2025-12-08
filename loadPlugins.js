072aa985ddbb6168d743d02d523931bb651063d7
CennoxX <cesar.bernard@gmx.de>
2025-12-08 22:24:23 +0100

    Update loadPlugins.js

--- loadPlugins.js
+++ loadPlugins.js
@@ -108,8 +108,9 @@ async function enrichGitHubData(plugin) {
         plugin.sourceCode = null;
       return plugin;
     }
-    console.log(`https://api.github.com/repos/${repo}/git/refs/heads/${default_branch}`);
-    var { object: { sha } } = await fetch(`https://api.github.com/repos/${repo}/git/refs/heads/${default_branch}`, { headers }).then(r => r.json());
+    var x = await fetch(`https://api.github.com/repos/${repo}/git/refs/heads/${default_branch}`, { headers }).then(r => r.json());
+    console.log(x);
+    var { object: { sha } } = x;
     var { tree } = await fetch(`https://api.github.com/repos/${repo}/git/trees/${sha}?recursive=1`, { headers }).then(r => r.json());
     var updateUrlPath = tree.find(i => i.path.match(/^(.*\.ver|(.*\.)?version|.*version.*\.(txt|info))$/i) || i.path.match(/Version$/))?.path;
     if (updateUrlPath) {
